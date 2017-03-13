7cc74a4f977f765bfe7eb44c11f52724bc767896
penggang@justbig.com <a121680>
2017-03-14 00:09:17 +0800

    in this class we will create a window and start loop

---
+++ game_function.py
@@ -0,0 +1,42 @@
+import sys
+import pygame
+
+
+def check_events(ship):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_ESCAPE:
+                sys.exit()
+            else:
+                check_key_down_event(event,ship)
+        elif event.type==pygame.KEYUP:
+            check_key_up_event(event,ship)
+
+
+
+def check_key_down_event(event,ship):
+    if event.key==pygame.K_RIGHT:
+        ship.moving_right=True
+    elif event.key==pygame.K_LEFT:
+        ship.moving_left=True
+    elif event.key==pygame.K_UP:
+        ship.moving_up=True
+    elif event.key==pygame.K_DOWN:
+        ship.moving_down=True
+
+def check_key_up_event(event,ship):
+    if event.key==pygame.K_RIGHT:
+        ship.moving_right=False
+    elif event.key==pygame.K_LEFT:
+        ship.moving_left=False
+    elif event.key==pygame.K_UP:
+        ship.moving_up=False
+    elif event.key==pygame.K_DOWN:
+        ship.moving_down=False
+
+def update_screen(ai_settings, screen, ship):
+    screen.fill(ai_settings.bg_color)
+    ship.biltme()
+    pygame.display.flip()
